b92566c02ff03c13e534a716055f72ab7723041e
Amine Salmane <amine.games2002@gmail.com>
2023-05-13 12:33:13 +0100

    starting

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 60px;
+  background-color: #2b2020;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>Navbar</Container>
+  )
+}
+
+export default Navbar
