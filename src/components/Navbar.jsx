5e61e0a8b148b0050a3b0554e082ba5f101e9762
Amine Salmane <amine.games2002@gmail.com>
2023-05-13 13:28:54 +0100

    steps

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,67 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  height: 60px;
-  background-color: #2b2020;
-`;
+import { Search } from "@mui/icons-material";
+import React from "react";
+import styled from "styled-components";
 
 const Navbar = () => {
   return (
-    <Container>Navbar</Container>
-  )
-}
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input/>
+            <Search/>
+          </SearchContainer>
+        </Left>
+        <Center>center</Center>
+        <Right>right</Right>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Navbar
+export default Navbar;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  height: 60px;
+  background-color: #ffffff;
+  
+`;
+
+const Left = styled.div`
+  flex : 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size:14PX;
+  cursor: pointer;
+`
+
+const SearchContainer = styled.div`
+  border :0.5px solid lightgrey;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`
+
+
+const Center = styled.div`
+  flex : 1;
+`;
+const Right = styled.div`
+  flex : 1;
+`;
