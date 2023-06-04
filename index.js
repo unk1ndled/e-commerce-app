0971b8314125f086a50a263a43e7fd583a6255ce
Amine Salmane <amine.games2002@gmail.com>
2023-06-04 16:47:40 +0100

    routes upodate

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const productRoute = require("./routes/products");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 dotenv.config();
 
@@ -21,7 +24,9 @@ app.get("/api/test", () => {
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
-
+app.use("/api/cart", cartRoute);
+app.use("/api/products", productRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("backend server is running");
